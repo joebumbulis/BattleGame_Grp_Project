c6c5d8d8c99d6ac4e2cedcd9446b0ad6b98c928a
Danielle Rodgers <Darodgers10@gmail.com>
2017-03-24 13:44:22 -0500

    updated jquery page one button function

---
+++ main.js
@@ -0,0 +1,9 @@
+// #start-btn function to switch to journey page
+
+  $('#start-btn').on('click', function() {
+    // $('.page-one-start-game').addClass('div-hide')
+    // $('.page-two').removeClass('div-hide')
+
+    $('.page-one-start-game').hide()
+    $('.page-two').show()
+  })
