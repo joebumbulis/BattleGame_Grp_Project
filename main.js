9d6f2194a84804b0e8e5d00a8333fdc773aaf3ca
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-24 12:49:27 -0500

    divhide changed to hidden

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var $playAgain = $('#play-again');
 
 $playAgain.on('click', function(e){
   console.log('yay');
-  $('.base').addClass('div-hide');
-  $('.page-one').removeClass('div-hide');
+  $('.base').addClass('hidden');
+  $('.page-one').removeClass('hidden');
 
 })
