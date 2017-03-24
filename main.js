0609ed80aa23e4047c26b2348eaea1f33efbb28b
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-24 14:13:27 -0500

    added event listener to p3

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+
+
+
+//**********************************************
+//Battle Page
+//when attack or defend button is pressed, page 3 display is none and page four appears.
+var $battleOver = $('.end')
+
+$battleOver.on('click', function(){
+  console.log('hi');
+  $('.base').addClass('hidden');
+  $('.page-four').removeClass('hidden');
+})
+
+
+
+//************************************************
+//End Page
 var $playAgain = $('#play-again');
 
 $playAgain.on('click', function(e){
