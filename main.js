280646c53ddbb106092aff1ff2fdb0fad0a65648
Rebekah Whittle <whittlebekah@gmail.com>
2017-03-24 12:32:22 -0500

    adding js logic and functionality

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+// function for journey page to move to the next page
+
+  // location option appears
+  // click location button
+    // weapon option appears
+  // click weapon button
+    // move on to battle page
+
+
+    // $albumTabs.on('click', function(e){
+    //   e.preventDefault();
+    //   var $albumContents = $('.album');
+    //   $albumContents.removeClass('selected');
+    //   var albumId = $(e.target).closest('a').attr('href');
+    //   $(albumId).addClass('selected');
+    //
+    //   $albumCover.addClass('hidden');
